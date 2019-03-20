68938bcb1fab44251222f1a377558b23b52e348b
xb215920 <p000caaraujo@prservicos.com.br>
2019-03-20 00:05:46 -0300

    front upmoney

---
+++ src/app/shared/sidebar/menu-items.ts
@@ -0,0 +1,116 @@
+import { RouteInfo } from './sidebar.metadata';
+
+export const ROUTES: RouteInfo[] = [
+  {
+    path: '',
+    title: 'Personal',
+    icon: 'mdi mdi-dots-horizontal',
+    class: 'nav-small-cap',
+    extralink: true,
+    submenu: []
+  },
+  {
+    path: '/starter',
+    title: 'Transações Consolidadas',
+    icon: 'mdi mdi-gauge',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '',
+    title: 'ADMINISTRAÇÃO',
+    icon: 'mdi mdi-dots-horizontal',
+    class: 'nav-small-cap',
+    extralink: true,
+    submenu: []
+  },
+  {
+    path: '/component/accordion',
+    title: 'Minha Conta',
+    icon: 'mdi mdi-equal',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/alert',
+    title: 'Cenários',
+    icon: 'mdi mdi-message-bulleted',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+/*  {
+    path: '/component/carousel',
+    title: 'Posição',
+    icon: 'mdi mdi-view-carousel',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/dropdown',
+    title: 'Dropdown',
+    icon: 'mdi mdi-calendar-clock',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/modal',
+    title: 'Modal',
+    icon: 'mdi mdi-tablet',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/pagination',
+    title: 'Pagination',
+    icon: 'mdi mdi-backburger',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/poptool',
+    title: 'Popover & Tooltip',
+    icon: 'mdi mdi-image-filter-vintage',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/progressbar',
+    title: 'Progressbar',
+    icon: 'mdi mdi-poll',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/rating',
+    title: 'Ratings',
+    icon: 'mdi mdi-bandcamp',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/tabs',
+    title: 'Tabs',
+    icon: 'mdi mdi-sort-variant',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/timepicker',
+    title: 'Timepicker',
+    icon: 'mdi mdi-calendar-clock',
+    class: '',
+    extralink: false,
+    submenu: []
+  }*/
+];
